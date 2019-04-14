2856f04c7328442b31d1fb11fc6100f1e99b5442
Dias Yesbay <34118218+ffellie@users.noreply.github.com>
2019-04-15 04:07:42 +0600

    'frontend'

--- week11/todofront/src/app/app.module.ts
+++ week11/todofront/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { MainService } from 'src/services/main.service';
+import { ProviderService } from './provider.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MainService,
+    ProviderService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
